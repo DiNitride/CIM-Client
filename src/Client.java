7ca0b0501ac950e24e7ee372504a2efca9f73813
DiNitride <j.ptb.t.ca+github@gmail.com>
2017-12-17 16:14:24 +0000

    Functional Client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,52 +8,67 @@ import java.net.Socket;
 
 public class Client extends JFrame {
 
-    private boolean         closed      = true;
-    private String          host        = "localhost";
-    private int             port        = 46400;
-    // private SendThread      sendLoop;
+    private boolean         closed          = true;
+    private String          host            = "localhost";
+    private int             port            = 46400;
     private ReceiveThread   receiveLoop;
     private Socket          clientSocket;
-    public  JTextArea       output;
-    public  JTextField      input;
-    public PrintWriter      outputStream;
+    private JTextArea       output;
+    private JTextField      input;
+    private JScrollPane     scrollOutput;
+    private PrintWriter     outputStream;
 
     public Client() {
-        setTitle("CIM");
+
+        setTitle("CIM Messenger");
+        ImageIcon img = new ImageIcon("resources/ico.png");
+        setIconImage(img.getImage());
         setSize(500, 500);
-        output = new JTextArea();
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         input = new JTextField(30);
+        input.setBorder(new EmptyBorder(10, 10, 10, 10));
         input.addActionListener(new EnterAction());
-        add(output, BorderLayout.CENTER);
+        output = new JTextArea();
+        output.setLineWrap(true);
+        output.setEditable(false);
+        output.setBorder(new EmptyBorder(10, 10, 10, 10));
+        scrollOutput = new JScrollPane(output);
+
+        add(scrollOutput, BorderLayout.CENTER);
         add(input, BorderLayout.SOUTH);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         setVisible(true);
         this.connect();
+
     }
 
     private void connect() {
+
         try {
             clientSocket = new Socket(this.host, this.port);
             this.closed = false;
             receiveLoop = new ReceiveThread();  receiveLoop.start();
-            // sendLoop    = new SendThread();     sendLoop.start();
             outputStream = new PrintWriter(this.clientSocket.getOutputStream(), true);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
     }
 
     class EnterAction implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-            // Implement sending data here
-            // Client.this.output.append(Client.this.input.getText());
             Client.this.outputStream.println(Client.this.input.getText());
+            Client.this.output.setCaretPosition(output.getDocument().getLength());
             Client.this.input.setText("");
         }
+
     }
 
     class ReceiveThread extends Thread implements Runnable {
+
         public void run() {
             try {
                 while (!Client.this.closed) {
@@ -67,22 +83,7 @@ public class Client extends JFrame {
                 ex.printStackTrace();
             }
         }
+
     }
-
-//    class SendThread extends Thread implements Runnable {
-//
-//        public void run() {
-//            try {
-//                PrintWriter socketOut = new PrintWriter(Client.this.clientSocket.getOutputStream(), true);
-//                BufferedReader sysIn = new BufferedReader(new InputStreamReader(System.in));
-//                while (!Client.this.closed) {
-//                    socketOut.println(sysIn.readLine());
-//                }
-//            } catch (IOException ex) {
-//                ex.printStackTrace();
-//            }
-//        }
-//    }
-
 }
 
