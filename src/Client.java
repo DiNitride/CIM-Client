720d05efba166e7d2c3888912f3440373c2a1c26
DiNitride <j.ptb.t.ca@gmail.com>
2018-01-19 12:52:52 +0000

    Caught connection error

--- src/Client.java
+++ src/Client.java
@@ -1,14 +1,23 @@
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
 
 public class Client extends JFrame {
 
-    private boolean         closed          = true;
+	private boolean         closed          = true;
     private String          host            = "localhost";
     private int             port            = 46400;
     private ReceiveThread   receiveLoop;
@@ -41,20 +50,30 @@ public class Client extends JFrame {
 
         setVisible(true);
         this.connect();
+        
 
     }
 
     private void connect() {
-
+    	
+    	addMessage("Attempting to connect to server at '" + this.host + "' . . .");
+    	
         try {
             clientSocket = new Socket(this.host, this.port);
             this.closed = false;
             receiveLoop = new ReceiveThread();  receiveLoop.start();
             outputStream = new PrintWriter(this.clientSocket.getOutputStream(), true);
+        } catch (java.net.ConnectException ex) {
+        	System.out.println("Error connecting");
+        	addMessage("Error connecting to server! Please retry");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
+    }
+    
+    public void addMessage(String message) {
+    	Client.this.output.append(message + "\n");
     }
 
     class EnterAction implements ActionListener {
@@ -76,7 +95,7 @@ public class Client extends JFrame {
                     String msg;
                     while ((msg = socketIn.readLine()) != null) {
                         System.out.println(msg);
-                        Client.this.output.append(msg + "\n");
+                        Client.this.addMessage(msg);
                     }
                 }
             } catch (IOException ex) {
