f3724072cdecd772352c48bd5e6b75b86c2c9f99
DiNitride <j.ptb.t.ca@gmail.com>
2018-03-19 20:20:03 +0000

    Comments

--- src/Client.java
+++ src/Client.java
@@ -8,6 +8,7 @@ import java.net.Socket;
 
 public class Client extends JFrame {
 
+    // Setting attributes of class
     private boolean         closed          = true;
     private String          host            = "localhost";
     private int             port            = 46400;
@@ -19,6 +20,8 @@ public class Client extends JFrame {
     private PrintWriter     outputStream;
 
     public Client() {
+        // Class Constructor
+        // Initialises Window and starts connection
 
         setTitle("CIM Messenger");
         ImageIcon img = new ImageIcon("resources/ico.png");
@@ -29,7 +32,7 @@ public class Client extends JFrame {
 
         input = new JTextField(30);
         input.setBorder(new EmptyBorder(10, 10, 10, 10));
-        input.addActionListener(new EnterAction());
+        input.addActionListener(new EnterAction()); // Adds event listener for enter action
         output = new JTextArea();
         output.setLineWrap(true);
         output.setEditable(false);
@@ -45,6 +48,7 @@ public class Client extends JFrame {
     }
 
     private void connect() {
+        // Connects to server and starts separate thread to receive data
 
         try {
             clientSocket = new Socket(this.host, this.port);
@@ -59,6 +63,10 @@ public class Client extends JFrame {
 
     class EnterAction implements ActionListener {
 
+        /*
+        Action Listener class to wait for key press and get data from the text input
+         */
+
         public void actionPerformed(ActionEvent e) {
             Client.this.outputStream.println(Client.this.input.getText());
             Client.this.output.setCaretPosition(output.getDocument().getLength());
@@ -69,9 +77,14 @@ public class Client extends JFrame {
 
     class ReceiveThread extends Thread implements Runnable {
 
+        /*
+        Separate class to handle repeatedly checking the connection for incoming data
+         */
+
         public void run() {
             try {
                 while (!Client.this.closed) {
+                    // Buffered reader keeps taking data until a new line character
                     BufferedReader socketIn = new BufferedReader(new InputStreamReader(Client.this.clientSocket.getInputStream()));
                     String msg;
                     while ((msg = socketIn.readLine()) != null) {
